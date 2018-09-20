fddfce4c422d885bcbbda6bd8010cef3a59e44f8
mclifford82 <mclifford82@gmail.com>
2018-09-20 01:01:22 -0700

    Color update

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Post
-
+# 9a21de5194abacbacb51c9c81d7e0a018f0a7976
 # Create your views here.
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
